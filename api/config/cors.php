2f47d02f3bd99d591269555021e713b65c51dc7a
mukira <mukira@lipagas.co.ke>
2024-05-29 02:04:19 +0000

    updated css

--- api/config/cors.php
+++ api/config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(['http://localhost:4200', env('CONSOLE_HOST'), Utils::addWwwToUrl(env('CONSOLE_HOST'))]),
+    'allowed_origins' => array_filter(['https://test.lipagas.co', 'https://test.lipagas.co:4200', 'https://test.lipagas.co:8000', 'https://test.lipagas.co:38000', env('CONSOLE_HOST'), Utils::addWwwToUrl(env('CONSOLE_HOST'))]),
 
     'allowed_origins_patterns' => [],
 
